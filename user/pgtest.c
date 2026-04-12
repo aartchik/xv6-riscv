c388326fd70a64082e77cef4e80673e11f8d0f56
artchik_mac <artyr-nasredin@yandex.ru>
2026-04-12 19:18:12 +0300

    fixed

--- user/pgtest.c
+++ user/pgtest.c
@@ -7,56 +7,26 @@
 #define BIGSIZE (BIGPAGES * PGSIZE)
 
 int g = 123;
-
-static void
-show(const char *name, void *addr, uint64 len)
-{
-  printf("%s: A=%d D=%d\n",
-         name,
-         pgcheck(addr, len, PTE_A),
-         pgcheck(addr, len, PTE_D));
-}
-
-static void
-show_all(const char *where, int *sp, char *sarr, int *hp, char *big)
-{
-  printf("-- %s --\n", where);
-  show("global int", &g, sizeof(g));
-  show("stack int", sp, sizeof(*sp));
-  show("stack array elem", &sarr[10], 1);
-  show("heap int", hp, sizeof(*hp));
-  show("heap big[0]", &big[0], 1);
-  show("heap big[4096]", &big[4096], 1);
-  show("heap big all", big, BIGSIZE);
-}
-
-static void
-clear_all(int *sp, char *sarr, int *hp, char *big)
-{
-  pgclear(&g, sizeof(g), PTE_A | PTE_D);
-  pgclear(sp, sizeof(*sp), PTE_A | PTE_D);
-  pgclear(&sarr[10], 1, PTE_A | PTE_D);
-  pgclear(hp, sizeof(*hp), PTE_A | PTE_D);
-  pgclear(big, BIGSIZE, PTE_A | PTE_D);
-}
+char garr[16] = { 0 };
 
 int
 main(int argc, char* argv[])
 {
   int s = 10;
-  char sarr[64];
+  char sarr[64] = { 0 };
   int* hp;
   char* big;
-  int x;
+  int x = 0;
 
-  memset(sarr, 0, sizeof(sarr));
+  sarr[10] = 11;
+  garr[5] = 22;
 
-  printf("=== at start ===\n");
+  printf("=== start ===\n");
   vmprint();
 
   hp = malloc(sizeof(int));
-  big = sbrk(BIGSIZE);
-  if (hp == 0 || big == SBRK_ERROR) {
+  big = malloc(3 * 4096);
+  if (hp == 0 || big == 0) {
     printf("alloc failed\n");
     exit(1);
   }
@@ -66,39 +36,79 @@ main(int argc, char* argv[])
   big[4096] = 2;
   big[8192] = 3;
 
-  printf("=== after allocation and init ===\n");
-  show_all("before clear", &s, sarr, hp, big);
+  printf("=== after alloc ===\n");
   vmprint();
 
-  clear_all(&s, sarr, hp, big);
-  show_all("after clear A/D", &s, sarr, hp, big);
+  printf("=== clear A/D ===\n");
+  if (pgclear(&g, sizeof(g), PTE_A | PTE_D) < 0 ||
+    pgclear(&garr[5], 1, PTE_A | PTE_D) < 0 ||
+    pgclear(&s, sizeof(s), PTE_A | PTE_D) < 0 ||
+    pgclear(&sarr[10], 1, PTE_A | PTE_D) < 0 ||
+    pgclear(hp, sizeof(int), PTE_A | PTE_D) < 0 ||
+    pgclear(big, 3 * 4096, PTE_A | PTE_D) < 0) {
+    printf("pgclear failed\n");
+    free(hp);
+    free(big);
+    exit(1);
+  }
   vmprint();
 
-  printf("=== read data ===\n");
-  x = 0;
+  printf("=== read ===\n");
   x += g;
+  x += garr[5];
   x += s;
   x += sarr[10];
   x += *hp;
   x += big[0];
   x += big[4096];
-  printf("read sum=%d\n", x);
-  show_all("after read", &s, sarr, hp, big);
+  x += big[8192];
+  printf("dummy=%d\n", x);
+
+  printf("g A = %d\n", pgcheck(&g, sizeof(g), PTE_A));
+  printf("garr A = %d\n", pgcheck(&garr[5], 1, PTE_A));
+  printf("s A = %d\n", pgcheck(&s, sizeof(s), PTE_A));
+  printf("sarr A = %d\n", pgcheck(&sarr[10], 1, PTE_A));
+  printf("hp A = %d\n", pgcheck(hp, sizeof(int), PTE_A));
+  printf("big A = %d\n", pgcheck(big, 3 * 4096, PTE_A));
   vmprint();
 
   printf("=== write data ===\n");
   g = 1;
-  s = 2;
-  sarr[10] = 3;
-  *hp = 4;
-  big[0] = 5;
-  big[4096] = 6;
-  show_all("after write", &s, sarr, hp, big);
+  garr[5] = 2;
+  s = 3;
+  sarr[10] = 4;
+  *hp = 5;
+  big[0] = 6;
+  big[4096] = 7;
+  big[8192] = 8;
+
+  printf("g D = %d\n", pgcheck(&g, sizeof(g), PTE_D));
+  printf("garr D = %d\n", pgcheck(&garr[5], 1, PTE_D));
+  printf("s D = %d\n", pgcheck(&s, sizeof(s), PTE_D));
+  printf("sarr D = %d\n", pgcheck(&sarr[10], 1, PTE_D));
+  printf("hp D = %d\n", pgcheck(hp, sizeof(int), PTE_D));
+  printf("big D = %d\n", pgcheck(big, 3 * 4096, PTE_D));
   vmprint();
 
-  printf("=== invalid arguments ===\n");
+  free(hp);
+  free(big);
+
+  printf("=== after free ===\n");
+  vmprint();
+
+  printf("=== invalid args ===\n");
   printf("bad flags clear = %d\n", pgclear(&g, sizeof(g), PTE_W));
   printf("bad flags check = %d\n", pgcheck(&g, sizeof(g), PTE_W));
+
+  printf("zero flags clear = %d\n", pgclear(&g, sizeof(g), 0));
+  printf("zero flags check = %d\n", pgcheck(&g, sizeof(g), 0));
+
+  printf("mixed flags clear = %d\n", pgclear(&g, sizeof(g), PTE_A | PTE_W));
+  printf("mixed flags check = %d\n", pgcheck(&g, sizeof(g), PTE_A | PTE_W));
+
+  printf("zero len clear = %d\n", pgclear(&g, 0, PTE_A));
+  printf("zero len check = %d\n", pgcheck(&g, 0, PTE_A));
+
   printf("bad addr clear = %d\n", pgclear((void*)MAXVA, 4, PTE_A));
   printf("bad addr check = %d\n", pgcheck((void*)MAXVA, 4, PTE_A));
 

13b4cdaa4d4ee30499bd467fc476c624873c1c11
aartchik <artyr-nasredin@yandex.ru>
2026-04-20 23:07:28 +0300

    try

--- user/date.c
+++ user/date.c
@@ -1,10 +1,30 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
+#define NSEC_PER_SEC 1000000000L
+#define SEC_PER_DAY 86400L
+#define DAYS_PER_400_YEARS 146097L
+
 static int
-is_leap(int y)
+is_leap(long y)
 {
   return (y % 4 == 0 && y % 100 != 0) || (y % 400 == 0);
+}
+
+static int
+days_in_year(long y)
+{
+  return is_leap(y) ? 366 : 365;
+}
+
+static int
+days_in_month(long y, int m)
+{
+  static int mdays[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+  if (m == 2 && is_leap(y))
+    return 29;
+  return mdays[m - 1];
 }
 
 static void
@@ -16,9 +36,10 @@ print2(int x)
 }
 
 static void
-print9(uint64 x)
+print9(long x)
 {
-  uint64 div = 100000000;
+  long div = 100000000L;
+
   while (div > 0) {
     printf("%d", (int)(x / div));
     x %= div;
@@ -26,48 +47,76 @@ print9(uint64 x)
   }
 }
 
+static long
+floor_div(long x, long y)
+{
+  long q = x / y;
+  long r = x % y;
+
+  if (r < 0)
+    q--;
+  return q;
+}
+
+static long
+floor_mod(long x, long y)
+{
+  long r = x % y;
+
+  if (r < 0)
+    r += y;
+  return r;
+}
+
+static void
+civil_from_days(long z, long *year, int *month, int *day)
+{
+  long y, era;
+  int m, mdays;
+
+  y = 1970;
+  era = floor_div(z, DAYS_PER_400_YEARS);
+  y += era * 400;
+  z -= era * DAYS_PER_400_YEARS;
+
+  while (z >= days_in_year(y)) {
+    z -= days_in_year(y);
+    y++;
+  }
+
+  m = 1;
+  while (z >= (mdays = days_in_month(y, m))) {
+    z -= mdays;
+    m++;
+  }
+
+  *year = y;
+  *month = m;
+  *day = z + 1;
+}
+
 int
 main(void)
 {
-  uint64 ns = rtctime();
-  uint64 s = ns / 1000000000ULL;
-  uint64 frac = ns % 1000000000ULL;
+  long ns, sec, nsec, days, daysec, year;
+  int month, day, hour, min;
 
-  int sec = s % 60;
-  s /= 60;
-  int min = s % 60;
-  s /= 60;
-  int hour = s % 24;
-  s /= 24;
+  ns = (long)rtctime();
+  sec = floor_div(ns, NSEC_PER_SEC);
+  nsec = floor_mod(ns, NSEC_PER_SEC);
 
-  int year = 1970;
+  days = floor_div(sec, SEC_PER_DAY);
+  daysec = floor_mod(sec, SEC_PER_DAY);
 
-  while (1) {
-    int days = is_leap(year) ? 366 : 365;
-    if (s >= (uint64)days) {
-      s -= days;
-      year++;
-    }
-    else {
-      break;
-    }
-  }
+  hour = daysec / 3600;
+  daysec %= 3600;
+  min = daysec / 60;
+  sec = daysec % 60;
 
-  int mdays[] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+  civil_from_days(days, &year, &month, &day);
 
-  if (is_leap(year))
-    mdays[1] = 29;
-
-  int month = 0;
-  while (s >= (uint64)mdays[month]) {
-    s -= mdays[month];
-    month++;
-  }
-
-  int day = s + 1;
-
-  printf("%d-", year);
-  print2(month + 1);
+  printf("%ld-", year);
+  print2(month);
   printf("-");
   print2(day);
   printf(" ");
@@ -77,7 +126,7 @@ main(void)
   printf(":");
   print2(sec);
   printf(".");
-  print9(frac);
+  print9(nsec);
   printf("\n");
 
   exit(0);
